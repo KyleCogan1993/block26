cab00dfb068400ababa06c347c8e56e73d983fac
Kyle Cogan <kyle.cogan1993@gmail.com>
2024-02-15 15:45:52 -0500

    selected contact view

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import './App.css'
 import ContactList from './components/ContactList'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedContactId, setSelectedContactId] = useState(null)
 
   return (
     <>
-      <ContactList />
+      {selectedContactId ? (<div>Selected Contact View</div>) : (<ContactList setSelectedContactId={setSelectedContactId}/>)}
     </>
-  )
+  );
 }
 
 export default App
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -3,13 +3,15 @@ import {useState} from 'react'
 import ContactRow from './ContactRow'
 import {useEffect} from 'react'
 
-export default function ContactList() {
+export default function ContactList({setSelectedContactId}) {
     const dummyContacts = [
         { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
         { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
         { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
       ];
+      
       const [contacts, setContacts] = useState(dummyContacts);
+      
       useEffect(() => {
         async function fetchContacts() {
           try {
@@ -24,6 +26,7 @@ export default function ContactList() {
         }
         fetchContacts();
       }, []);
+
     return (
         <table>
             <thead>
@@ -39,7 +42,7 @@ export default function ContactList() {
             </tr>
             {
                 contacts.map(contact => {
-                    return <ContactRow key={contact.id} contact={contact} />;
+                    return <ContactRow setSelectedContactId={setSelectedContactId} key={contact.id} contact={contact} />;
                 })
             }
             </tbody>
